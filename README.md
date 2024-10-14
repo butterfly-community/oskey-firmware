e2cf519ed5a1066459662d83bd4f28d132a9d389
Zhang WenChao <zhangwenchao001@gmail.com>
2024-10-15 00:23:33 +0800

    feat: bip39 support

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Free hardware wallet allows everyone to use the blockchain safely.
+
+Already BIP39 use hardware cryptographically secure random:
+
+```
+
+*** Booting Zephyr OS build v3.7.0-4339-g1ec5ce05f9f8 ***
+Mnemonic: 128 bit: winner organ tank control marriage fade model opinion naive chalk hen final
+```
--- src/main.c
+++ src/main.c
@@ -1,14 +1,32 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
+#include "crypto/bip39.h"
+#include <zephyr/random/random.h>
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+  const char *mnemonic = mnemonic_generate(128);
 
-	return 0;
+  printf("Mnemonic: 128 bit: %s\n", mnemonic);
+
+  return 0;
 }
+
+#ifdef CONFIG_ENTROPY_HAS_DRIVER
+
+uint32_t random32(void)
+{
+  uint32_t ret;
+
+  sys_csrand_get(&ret, sizeof(ret));
+
+  return ret;
+}
+
+#else
+
+uint32_t random32(void)
+{
+  return sys_rand32_get();
+}
+
+#endif /* CONFIG_ENTROPY_HAS_DRIVER */
