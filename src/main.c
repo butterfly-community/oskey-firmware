4b9c0f7437b3f6d78852a2c666f53bce276746ff
Zhang WenChao <zhangwenchao001@gmail.com>
2024-10-17 09:22:35 +0800

    feat: add mnemonic too seed

--- src/main.c
+++ src/main.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
-#include "crypto/bip39.h"
 #include <zephyr/random/random.h>
+#include "crypto/bip39.h"
 
 int main(void)
 {
   const char *mnemonic = mnemonic_generate(128);
 
-  printf("Mnemonic: 128 bit: %s\n", mnemonic);
+  uint8_t seed[512 / 8];
+
+  printf("\nMnemonic: 128 bit: %s\n", mnemonic);
+
+  mnemonic_to_seed(mnemonic, "", seed, NULL);
+
+  printf("Seed: ");
+
+  for (int i = 0; i < sizeof(seed); i++)
+  {
+    printf("%02x", seed[i]);
+  }
 
   return 0;
 }
 
-uint32_t random32(void) {
+uint32_t random32(void)
+{
 #ifdef CONFIG_ENTROPY_HAS_DRIVER
-    uint32_t ret;
-    sys_csrand_get(&ret, sizeof(ret));
-    return ret;
+  uint32_t ret;
+  sys_csrand_get(&ret, sizeof(ret));
+  return ret;
 #else
-    return sys_rand32_get();
+  return sys_rand32_get();
 #endif
 }
