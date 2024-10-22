157e6a2803f06e7e18ddcbebb1a7cc2bef73cad3
Zhang WenChao <zhangwenchao001@gmail.com>
2024-10-22 13:12:53 +0800

    feat: crypto to rust lib and rust toolchain update

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,39 @@
 #![no_std]
 
+extern crate alloc;
+extern crate zephyr;
+
+use alloc::format;
+use alloc::{string::String, vec::Vec};
+use bip39;
 use zephyr::printkln;
 
-extern crate zephyr;
+extern "C" {
+    fn cs_random(dst: *mut u8, len: usize);
+}
+
+/// Crypto safe random rust wrapper
+pub fn rust_cs_random_vec(len: usize) -> Vec<u8> {
+    let mut buffer = Vec::with_capacity(len);
+    unsafe {
+        buffer.set_len(len);
+        cs_random(buffer.as_mut_ptr(), len);
+    }
+    buffer
+}
 
 #[no_mangle]
 extern "C" fn rust_main() {
-    printkln!("Test Rust Support {}", zephyr::kconfig::CONFIG_BOARD);
+    // Get random
+    let entropy = rust_cs_random_vec(16);
+    // Create Mnemonic
+    let mnemonic = bip39::Mnemonic::from_entropy(&entropy).unwrap();
+    // Print mnemonic
+    let mnemonic_string = mnemonic.words().collect::<Vec<_>>().join(" ");
+    printkln!("Mnemonic: {}", mnemonic_string);
+    // Mnemonic to seed with null passphrase
+    let seed = mnemonic.clone().to_seed("");
+    let seed_hex_string: String = seed.iter().map(|&byte| format!("{:02x}", byte)).collect();
+    // Print seed
+    printkln!("Seed: {}", seed_hex_string);
 }
--- src/main.c
+++ src/main.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/random/random.h>
-#include "bip39.h"
-
-#ifdef CONFIG_RUST
+#include <psa/crypto.h>
+#include "temp/test.h"
 
 extern void rust_main(void);
 
 int main(void)
 {
 	rust_main();
-
-	const char *mnemonic = mnemonic_generate(128);
-
-	uint8_t seed[512 / 8];
-
-	printf("\nMnemonic: 128 bit: %s\n", mnemonic);
-
-	mnemonic_to_seed(mnemonic, "", seed, NULL);
-
-	for (int i = 0; i < sizeof(seed); i++)
-	{
-		printf("%02x", seed[i]);
-	}
-	printf("\n\n");
-
+	test();
 	return 0;
 }
 
-uint32_t random32(void)
+void cs_random(void *dst, size_t len)
 {
-	uint32_t ret;
-	sys_csrand_get(&ret, sizeof(ret));
-	return ret;
+	sys_csrand_get(dst, len);
 }
 
 void rust_panic_wrap(void)
 {
 	k_panic();
 }
-
-#endif
