b26ea59cdbe4f787b2746f728651b220e64d060b
Zhang WenChao <zhangwenchao001@gmail.com>
2025-10-07 18:48:30 +0800

    feat: update boards config

--- src/gpio.c
+++ src/gpio.c
@@ -17,7 +17,7 @@ static struct gpio_callback button_cb_data;
 static void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
 	k_work_submit(&app_sign_work);
-	LOG_INF("User button pressed!");
+	LOG_INF("User button released!");
 }
 
 bool user_button_exists(void)
@@ -36,13 +36,13 @@ int user_button_init(void)
 		return ret;
 	}
 
-	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_INACTIVE);
 	if (ret) {
 		return ret;
 	}
-
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
 
 	return 0;
 }
