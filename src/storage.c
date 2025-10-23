52ce1604a037f25ae3817c8eb783fe69b555a35d
Zhang WenChao <zhangwenchao001@gmail.com>
2025-10-23 09:02:08 +0800

    feat: change nvs to zms
    close: #30

--- src/storage.c
+++ src/storage.c
@@ -1,9 +1,9 @@
 #include "storage.h"
 
-#ifdef CONFIG_NVS
+#ifdef CONFIG_ZMS
 
 #include <zephyr/kernel.h>
-#include <zephyr/fs/nvs.h>
+#include <zephyr/fs/zms.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
@@ -15,12 +15,12 @@
 
 #endif
 
-#define NVS_PARTITION        storage_partition
-#define NVS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(NVS_PARTITION)
-#define NVS_PARTITION_OFFSET FIXED_PARTITION_OFFSET(NVS_PARTITION)
-#define NVS_PARTITION_SIZE   FIXED_PARTITION_SIZE(NVS_PARTITION)
+#define ZMS_PARTITION        storage_partition
+#define ZMS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(ZMS_PARTITION)
+#define ZMS_PARTITION_OFFSET FIXED_PARTITION_OFFSET(ZMS_PARTITION)
+#define ZMS_PARTITION_SIZE   FIXED_PARTITION_SIZE(ZMS_PARTITION)
 
-static struct nvs_fs fs;
+static struct zms_fs fs;
 
 bool ohw_official = false;
 
@@ -28,33 +28,37 @@ int storage_init()
 {
 	struct flash_pages_info info;
 
-	fs.flash_device = NVS_PARTITION_DEVICE;
-	int res = device_is_ready(fs.flash_device);
+	fs.flash_device = ZMS_PARTITION_DEVICE;
+	if (!device_is_ready(fs.flash_device)) {
+		return -ENODEV;
+	}
+
+	fs.offset = ZMS_PARTITION_OFFSET;
+	int res = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &info);
 	if (res < 0) {
 		return res;
 	}
 
-	fs.offset = NVS_PARTITION_OFFSET;
-	res = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &info);
-	if (res < 0) {
-		return res;
-	}
-
-	if (info.size > UINT16_MAX || info.size == 0 || NVS_PARTITION_SIZE < 1024) {
+	if (info.size == 0 || ZMS_PARTITION_SIZE < 1024) {
 		return -EINVAL;
 	}
 
 	fs.sector_size = info.size;
-	fs.sector_count = (NVS_PARTITION_SIZE / fs.sector_size);
+	fs.sector_count = (ZMS_PARTITION_SIZE / fs.sector_size);
 
-	// printk("NVS Configuration:\n");
+	// ZMS requires minimum 2 sectors
+	if (fs.sector_count < 2) {
+		return -EINVAL;
+	}
+
+	// printk("ZMS Configuration:\n");
 	// printk("  Flash device: %p (%s)\n", fs.flash_device, fs.flash_device->name);
 	// printk("  Offset: 0x%lx (%ld)\n", fs.offset, fs.offset);
 	// printk("  Sector size: %d bytes\n", fs.sector_size);
 	// printk("  Sector count: %d\n", fs.sector_count);
 	// printk("  Total size: %d bytes\n", fs.sector_size * fs.sector_count);
 
-	res = nvs_mount(&fs);
+	res = zms_mount(&fs);
 	if (res < 0) {
 		return res;
 	}
@@ -78,7 +82,7 @@ int storage_init()
 bool storage_general_check(uint16_t id)
 {
 	uint8_t read_value[2] = {0};
-	int res = nvs_read(&fs, id, &read_value, sizeof(read_value));
+	int res = zms_read(&fs, id, &read_value, sizeof(read_value));
 	if (res < 0) {
 		return false;
 	}
@@ -87,7 +91,7 @@ bool storage_general_check(uint16_t id)
 
 bool storage_general_write(const uint8_t *data, int len, uint16_t id)
 {
-	int res = nvs_write(&fs, id, data, len);
+	int res = zms_write(&fs, id, data, len);
 	if (res < 0) {
 		return false;
 	}
@@ -96,17 +100,17 @@ bool storage_general_write(const uint8_t *data, int len, uint16_t id)
 
 int storage_general_read(uint8_t *data, size_t len, uint16_t id)
 {
-	return nvs_read(&fs, id, data, len);
+	return zms_read(&fs, id, data, len);
 }
 
 int storage_erase_nvs()
 {
-	return nvs_clear(&fs);
+	return zms_clear(&fs);
 }
 
 int storage_delete(uint16_t id)
 {
-	return nvs_delete(&fs, id);
+	return zms_delete(&fs, id);
 }
 
 int storage_erase_flash()
